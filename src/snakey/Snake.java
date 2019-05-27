f3557d7b8451f4dd9760ebc95a542df6e2519871
czarkom <mczarkowski@onet.pl>
2019-05-27 22:50:58 +0200

    JFrame creation

--- src/snakey/Snake.java
+++ src/snakey/Snake.java
@@ -1,7 +1,28 @@
 package snakey;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Snake {
+
+    public JFrame gameFrame;
+
+    public static Snake snake;
+    //public Toolkit toolkit;
+
+    public Snake(){
+        //toolkit = Toolkit.getDefaultToolkit();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        gameFrame = new JFrame("Snakey");
+        gameFrame.setVisible(true);
+        gameFrame.setSize(800,800);
+        gameFrame.setLocation(dim.width/2 - gameFrame.getWidth()/2, dim.height/2 - gameFrame.getHeight()/2);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+
     public static void main(String[] args){
-        System.out.println("Hello World");
+        snake = new Snake();
     }
 }
