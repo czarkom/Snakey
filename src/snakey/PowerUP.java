d8b21e5b0ea5dd15b83f8f5a86c185b7dc6d190f
ozzyfilip <ozzyfilip@gmail.com>
2019-05-28 22:00:57 +0200

    Modify powerups

--- src/snakey/PowerUP.java
+++ src/snakey/PowerUP.java
@@ -9,9 +9,11 @@ public class PowerUP {
     Type powerUpType;
     Random random = new Random();
     Point cord;
+    Snake snakey;
     int counterForPowerUps;
 
     public PowerUP(Snake snake) {
+        snakey = snake;
         cord = new Point(random.nextInt(snake.dim.width / snake.SCALE), random.nextInt(snake.dim.height / snake.SCALE));
         double tempNumberOfRand = random.nextDouble() % 10;
         if (tempNumberOfRand <= 8) {
@@ -21,6 +23,34 @@ public class PowerUP {
         } else {
             powerUpType = Type.SPEED;
         }
+        counterForPowerUps = 0;
 
+    }
+
+    public void remake() {
+        cord = new Point(random.nextInt(snakey.dim.width / snakey.SCALE), random.nextInt(snakey.dim.height / snakey.SCALE));
+        if (counterForPowerUps != 5) {
+            double tempNumberOfRand = random.nextDouble() % 10;
+            if (tempNumberOfRand <= 8) {
+                powerUpType = Type.POINT;
+                counterForPowerUps++;
+            } else if (tempNumberOfRand <= 9) {
+                powerUpType = Type.SLOW;
+                counterForPowerUps = 0;
+            } else {
+                powerUpType = Type.SPEED;
+                counterForPowerUps = 0;
+            }
+        } else {
+            double tempNumberOfRand = random.nextDouble() % 10;
+
+            if (tempNumberOfRand <= 5) {
+                powerUpType = Type.SLOW;
+                counterForPowerUps = 0;
+            } else {
+                powerUpType = Type.SPEED;
+                counterForPowerUps = 0;
+            }
+        }
     }
 }
